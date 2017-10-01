6f0b21c85458582ef03b0597e40a505b007a1138
Nikita Timonin <nikita.v.timonin@gmail.com>
2017-10-01 17:00:08 +0300

    Initial Commit

---
+++ KeyAdressGen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyAdressGen
+//
+//  Created by Nikita Timonin on 01/10/2017.
+//  Copyright © 2017 Palatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
